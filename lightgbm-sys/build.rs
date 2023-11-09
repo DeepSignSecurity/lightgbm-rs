193e2db87c45dc1b6b0b5ec12025a7ca1606848c
leofidus <jannis.froese@deepsign.de>
2023-11-09 11:44:11 +0100

    update bindgen (#14)

--- lightgbm-sys/build.rs
+++ lightgbm-sys/build.rs
@@ -45,6 +45,7 @@ fn main() {
 
     // bindgen build
     let bindings = bindgen::Builder::default()
+        .size_t_is_usize(true)
         .header("wrapper.h")
         .clang_args(&["-x", "c++", "-std=c++11"])
         .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
